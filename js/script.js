024ba163371a8680343b3adaf11904abab5f2bfc
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-12-21 17:26:01 +0200

    updated website to reflect changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // function to set a given theme/color-scheme
- function setTheme(themeName) {
+function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
